5fb389cf4a2ec551fcf3d6ad2089d8992afdf127
Sammymac <samrmc@gmail.com>
2018-06-16 13:24:14 +0100

    Added wall physics and restricting movement to be within the player radius only

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,21 +9,29 @@ public class PlayerController : MonoBehaviour {
 
     // Private variables
     private Rigidbody2D rb;
+    private Collider2D col;
     private Vector2 mousePos;
     private bool hasFired = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0)){
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
             print("Mouse pressed");
             mousePos = Input.mousePosition;
         } else if (Input.GetMouseButtonUp(0)) {
-            hasFired = true;
+            Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector2 mouseWorldPos = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
+            print("Mousepos:" + mouseWorldPos + ", bounds:" + col.OverlapPoint(mouseWorldPos));
+            if (col.bounds.Contains(mouseWorldPos)) {
+                hasFired = true;
+                print("Fired");
+            }
             print("Mouse released");
         }
     }
@@ -33,10 +41,11 @@ public class PlayerController : MonoBehaviour {
             Vector2 movement = Input.mousePosition;
             Vector2 difference = movement - mousePos;
 
-            print("Movement:"+movement);
+            print("Movement:" + movement);
             print("Difference:" + difference);
             rb.AddForce(difference * speed, ForceMode2D.Impulse);
             hasFired = false;
         }
     }
+    
 }
