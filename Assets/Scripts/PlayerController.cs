f8ab015c0919db17218f21d0d5b8c489de0f5e70
Sammymac <samrmc@gmail.com>
2018-09-23 16:10:32 +0100

    Cursor points and scales correctly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,9 @@ public class PlayerController : MonoBehaviour
     // Public variables
     public float speed;
     public GameObject cursorTemplate;
-    public Camera cam;
 
     // Private variables
+    private Camera cam;
     private Rigidbody2D rb;
     private Collider2D col;
     private Vector2 mousePos;
@@ -22,20 +22,16 @@ public class PlayerController : MonoBehaviour
         //Cursor.visible = false; // FIXME Uncomment this when testing finished
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rb.IsSleeping()) {
+//        if (rb.IsSleeping()) {
             if (Input.GetMouseButtonDown(0)) {
                 mousePos = Input.mousePosition;
                 if (IsClickWithinPlayerRadius(mousePos)) {
-                    //mousePos = Input.mousePosition;
-                    //mousePos = cam.WorldToScreenPoint(col.bounds.center);
-
-                    print("Position:" + transform.position);
-                    print("Input.mousePosition:" + Input.mousePosition);
                     aiming = true;
                 }
             } else if (Input.GetMouseButtonUp(0)) {
@@ -43,7 +39,7 @@ public class PlayerController : MonoBehaviour
                     hasFired = true;
                 }
             }
-        }
+//        }
     }
 
     private void FixedUpdate()
@@ -52,9 +48,8 @@ public class PlayerController : MonoBehaviour
         Vector2 currentMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
 
         if (hasFired) {
-            Vector2 difference = currentMousePosition - new Vector2(transform.position.x, transform.position.y);
+            var difference = currentMousePosition - new Vector2(transform.position.x, transform.position.y);
             
-            //difference = newMousePos - col.bounds.center;
             print("Fired");
 
             rb.AddForce(difference * speed, ForceMode2D.Impulse);
@@ -71,40 +66,32 @@ public class PlayerController : MonoBehaviour
                 print("currentMousePosition:"+currentMousePosition);
             }
             
-            // Aiming Code
-            
-            Vector3 object_pos = Camera.main.WorldToScreenPoint(currentPlayerPosition);
-            Vector3 mouse_pos = Input.mousePosition;
-            mouse_pos.x = mouse_pos.x - object_pos.x;
-            mouse_pos.y = mouse_pos.y - object_pos.y;
-            float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-            cursor.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            
-//            cursor.transform.right = currentMousePosition - currentPlayerPosition;
-//            cursor.transform.LookAt(currentMousePosition);
-
-            // Scaling code
-            Vector3 objectScale = cursor.transform.localScale;
-            float distance = Vector3.Distance(currentPlayerPosition, currentMousePosition);
-            float x = cursor.GetComponent<Collider2D>().bounds.size.x;
-
-//            Vector3 newScale = new Vector3(distance/(x*2), distance/(x*2), 0);
-//            cursor.transform.localScale = newScale;
-
-//            print("Distance:" + distance);
-//            print("x:" + x);
-//            print("objectScale:" + objectScale);
-//            print("newScale:" + newScale);
+            RotateCursorToMousePos();
+            ScaleCursorToMousePos();
         }
 
-
-
     }
-    
+
+    private void ScaleCursorToMousePos() {
+        Vector2 currentPlayerPosition = new Vector3(transform.position.x, transform.position.y);
+        Vector2 currentMousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        var distance = Vector3.Distance(currentPlayerPosition, currentMousePosition);
+        cursor.transform.localScale = new Vector3(distance/2.5F, 1, 0);
+    }
+
+    private void RotateCursorToMousePos(){
+        var currentPlayerPosition = cam.WorldToScreenPoint(transform.position);
+        var mousePosition = Input.mousePosition;
+        mousePosition.x = mousePosition.x - currentPlayerPosition.x;
+        mousePosition.y = mousePosition.y - currentPlayerPosition.y;
+        var angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        cursor.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
 
     private bool IsClickWithinPlayerRadius(Vector2 position) {
-        Vector3 mouseWorldPos3D = Camera.main.ScreenToWorldPoint(position);
-        Vector2 mouseWorldPos = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
+        var mouseWorldPos3D = cam.ScreenToWorldPoint(position);
+        var mouseWorldPos = new Vector2(mouseWorldPos3D.x, mouseWorldPos3D.y);
+        print(mouseWorldPos);
         return col.bounds.Contains(mouseWorldPos);
     }
 
