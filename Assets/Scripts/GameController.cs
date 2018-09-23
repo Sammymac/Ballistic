ed6c418cba52ca6ce1c2c0a81108a091b897f69d
Sammymac <samrmc@gmail.com>
2018-09-23 19:58:38 +0100

    Started tthe 'Game Over' functionality

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text gameOverText;
+	
+	private GameObject[] players;
+	private Boolean isGameOver = false;
+	
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isGameOver && GameObject.FindGameObjectsWithTag("Player").Length <= 1) {
+			Instantiate(gameOverText);
+			isGameOver = true;
+			// TODO Game Over
+		}
+	}
+}
