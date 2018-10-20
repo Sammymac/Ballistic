4b9c1513562b58eaa198b77c21d7e254f2730855
Sammymac <samrmc@gmail.com>
2018-10-20 17:15:50 +0100

    Added Level button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,29 +1,46 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+    public static GameController instance = null;
+    private int level = 1;
 
-	public Text gameOverText;
-	
-	private GameObject[] players;
-	private Boolean isGameOver = false;
-	
-	// Use this for initialization
-	void Start () {
-		players = GameObject.FindGameObjectsWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Text gameOverText;
 
-		if (!isGameOver && GameObject.FindGameObjectsWithTag("Player").Length <= 1) {
-			Instantiate(gameOverText);
-			isGameOver = true;
-			// TODO Game Over
-		}
-	}
+    private GameObject[] players;
+    private Boolean isGameOver = false;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start() {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isGameOver && GameObject.FindGameObjectsWithTag("Player").Length <= 0) {
+            Instantiate(gameOverText);
+            isGameOver = true;
+        }
+    }
+    
+    public void GotoLevel1() {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void GotoLevel2() {
+        SceneManager.LoadScene("level2");
+    }
 }
