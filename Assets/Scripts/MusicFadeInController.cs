7fe037adbb7236f1aaf954896f3902d7178ac255
Sammymac <samrmc@gmail.com>
2018-11-25 13:14:39 +0000

    Added Music, moved some common objects into a single prefab

---
+++ Assets/Scripts/MusicFadeInController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicFadeInController : MonoBehaviour {
+	[SerializeField] private int fadeInTime = 3;
+	private AudioSource audioSource;
+
+	private void Awake() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	private void Update() {
+		if (audioSource.volume < 0.1) {
+			audioSource.volume += (Time.deltaTime / (fadeInTime*10));
+		} else {
+			Destroy(this);
+		}
+	}
+}
